32de4c722e12147a202f554ea63750516704291e
lsamon <louis.amon@amazutech.net>
2017-07-15 02:15:10 +1000

    added redirect after sending

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,24 +1,15 @@
-# if Rails.env.production?
-#
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: ENV["REDISTOGO_URL"], namespace: :resque }
-#   end
-#
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: ENV["REDISTOGO_URL"], namespace: :resque }
-#
-#     database_url = ENV['DATABASE_URL']
-#     if database_url
-#       ENV['DATABASE_URL'] = "#{database_url}?pool=250"
-#       ActiveRecord::Base.establish_connection
-#     end
-#   end
-# else
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: 'redis://localhost:6379/0', namespace: "simplesettler_sidekiq_#{Rails.env}" }
-#   end
-#
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: 'redis://localhost:6379/0', namespace: "simplesettler_sidekiq_#{Rails.env}" }
-#   end
-# end
+require 'sidekiq/web'
+
+Sidekiq.configure_server do |config|
+  ActiveRecord::Base.configurations[Rails.env.to_s]['pool'] = 30
+end
+
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV["REDISTOGO_URL"]}
+  end
+  
+  Sidekiq.configure_client do |config|
+   config.redis = { url: ENV["REDISTOGO_URL"]}
+  end
+end
