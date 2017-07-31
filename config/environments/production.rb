6b36dcc151d1bff3fb131a23372b6f6471e5f97b
lsamon <louis.amon@amazutech.net>
2017-07-31 22:18:36 +1000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # if ENV["RAILS_LOG_TO_STDOUT"].present?
   #   logger = ActiveSupport::Logger.new(STDOUT)
