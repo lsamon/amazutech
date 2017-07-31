6d613ada1caf8e83c2b6e882be94f2e7afc8dd14
lsamon <louis.amon@amazutech.net>
2017-07-31 21:14:46 +1000

    fixed sidekiq route issues

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'figaro'
+
 # Sidekiq for delayed jobs
 gem 'sidekiq'
 gem 'redis'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq'
   root to: "pages#index"
   resources :contact_submissions, only: [:new, :create]
 end
