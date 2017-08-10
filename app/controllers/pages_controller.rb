4872501418479579fb0bc672ce18e5539446460f
lsamon <louis.amon@amazutech.net>
2017-08-10 14:02:53 +1000

    removed some settings for page_controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,30 +1,12 @@
 class PagesController < ApplicationController
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/js' }
-  before_action :initialize_contact_submission, only: [:index, :submit_form]
+  before_action :find_contact_submission, only: [:index]
 
   def index
-    @latest_submission = ContactSubmission.by_ip(request.remote_ip)
   end
-
-  def submit_form
-    @contact_submission.contact_ip_address = request.remote_ip
-    respond_to do |format|
-      if @contact_submission.save
-        format.js   { }
-        format.json { render :index, status: :created, location: @contact_submission }
-      else
-        render :index
-      end
-    end
-  end
-
 
   private
-  def contact_submission_params
-    params.fetch(:contact_submission, {}).permit(:name, :email, :subject, :message)
-  end
-
-  def initialize_contact_submission
-    @contact_submission = ContactSubmission.new(contact_submission_params)
+  def find_contact_submission
+    @latest_submission = ContactSubmission.by_ip(request.remote_ip)
   end
 end
