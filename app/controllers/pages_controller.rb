8ef1a866649ba0872ac16e6f2cac7eb4ae79feb6
lsamon <louis.amon@amazutech.net>
2017-08-10 14:54:07 +1000

    added back content for pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,30 @@
 class PagesController < ApplicationController
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/js' }
-  before_action :find_contact_submission, :initialize_contact_submission, only: [:index]
+  before_action :initialize_contact_submission, only: [:index, :submit_form]
 
   def index
+    @latest_submission = ContactSubmission.by_ip(request.remote_ip)
   end
+
+  def submit_form
+    @contact_submission.contact_ip_address = request.remote_ip
+    respond_to do |format|
+      if @contact_submission.save
+        format.js   { }
+        format.json { render :index, status: :created, location: @contact_submission }
+      else
+        render :index
+      end
+    end
+  end
+
 
   private
   def contact_submission_params
     params.fetch(:contact_submission, {}).permit(:name, :email, :subject, :message)
   end
-  
+
   def initialize_contact_submission
     @contact_submission = ContactSubmission.new(contact_submission_params)
-  end
-
-  def find_contact_submission
-    @latest_submission = ContactSubmission.by_ip(request.remote_ip)
   end
 end
