f568b924b8887a06148786c0e86175e643b6963c
lsamon <louis.amon@amazutech.net>
2017-08-10 15:07:09 +1000

    changed route file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
   root to: "pages#index"
-  resources :contact_submissions, only: [:new, :create]
+  # resources :contact_submissions, only: [:new, :create]
 
-  post '/', to: 'pages#submit_form', as: 'form_submission', defaults: { format: 'js' }
+  post '/', to: 'contact_submissions#create', as: 'form_submission', defaults: { format: 'js' }
 end
