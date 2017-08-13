04feb381926140de8cd153e8d214a5b5dbc0f693
lsamon <louis.amon@amazutech.net>
2017-08-13 20:07:17 +1000

    made changes to the contact view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
-  root to: "pages#index"
-  resources :contact_submissions, only: [:index, :create], defaults: { format: 'js' }
+  root to: 'pages#index'
+  resources :contact_submissions, only: [:create], defaults: { format: 'js' }
 end
