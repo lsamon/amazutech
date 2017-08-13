02998d108d79f498f63017b47de39acd832bcd08
lsamon <louis.amon@amazutech.net>
2017-08-13 15:39:48 +1000

    edited routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
   root to: "pages#index"
-  resources :contact_submissions, only: [:new, :create], defaults: { format: 'js' }
+  resources :contact_submissions, only: [:create], defaults: { format: 'js' }
 end
