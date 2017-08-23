d231ff086aed936006728547a7af14926d21b4e6
lsamon <louis.amon@amazutech.net>
2017-08-24 00:31:11 +1000

    added logo on mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
   def form_enquiry(contact)
     @contact = contact
+    attachments.inline['logo.png'] = File.read('app/assets/images/logo.png')
     @email_subject = "Webform Enquiry"
     mail(to: 'loui.amon@gmail.com', subject: @email_subject)
   end
