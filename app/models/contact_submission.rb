1bbae29a8cf45bdea90fc85b0018164202902298
lsamon <louis.amon@amazutech.net>
2017-09-07 18:59:02 +1000

    added simple SPAM check feature

--- app/models/contact_submission.rb
+++ app/models/contact_submission.rb
@@ -1,6 +1,9 @@
 class ContactSubmission < ApplicationRecord
+  attr_accessor :comment # fake attribute used for spam trapping
+
   validates :message, presence: true
   validates :email, email: true
+  validates_length_of :comment, :in => 0..1
 
   def self.by_ip(ip)
     ContactSubmission.where(contact_ip_address: ip).order('id').last
