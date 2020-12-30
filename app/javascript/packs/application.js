2855b6eaaef2bf3ed148284fd999024b287637a2
Louis Amon <louis.amon@amazutech.net>
2020-12-31 00:20:06 +1100

    Fixing remove bootstrap in favor of my own styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,12 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+// CSS
+import '../stylesheets/style.scss';
+
+// JS
+import "@fortawesome/fontawesome-free/js/all";
+
+require("jquery");
+require("bootstrap");
+
