fb8b47b941e2664f4777ae81d02a0b48d7be3bac
Louis Amon <louis.amon@amazutech.net>
2021-06-13 18:45:49 +1000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
   root to: 'pages#index'
-  post '/submit_form' => 'pages#contact_us'
+  post '/submit_form' => 'pages#submit_form'
   # resources :contact_submissions, only: [:], defaults: { format: 'js' }
 end
