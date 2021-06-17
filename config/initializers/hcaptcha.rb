4b56550006c2b0c692ba9c86c5b077266b265025
Louis Amon <louis.amon@amazutech.net>
2021-06-18 00:21:30 +1000

    Change hcaptcha environment variables

--- config/initializers/hcaptcha.rb
+++ config/initializers/hcaptcha.rb
@@ -3,6 +3,6 @@
 return unless Rails.env.production?
 
 Hcaptcha.configure do |config|
-  config.site_key = Figaro.env.hcaptcha.site_key
-  config.secret_key = Figaro.env.hcaptcha.site_secret
+  config.site_key = Figaro.env.hcaptcha_site_key
+  config.secret_key = Figaro.env.hcaptcha_site_secret
 end
